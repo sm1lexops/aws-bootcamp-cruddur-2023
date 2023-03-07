3555c085b95c4c4fd803e130c91f6ced29597567
sm1lexops <smiledevops@gmail.com>
2023-03-07 12:53:17 +0000

    Implement to app.py SimpleSpan and ConsoleExporter

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,7 +6,7 @@ class HomeActivities:
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
+      span.set_attribute("app.hubabuba", now.isoformat())
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Smirnov Aleksey',
@@ -46,5 +46,5 @@ class HomeActivities:
       'replies': []
     }
     ]
-    span.set_attribute("app.result_length", len(results))
+    span.set_attribute("app.result_length", now.date())
     return results
