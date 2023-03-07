ca4a6f6bac200b702f3b112da941966ea2607782
sm1lexops <smiledevops@gmail.com>
2023-03-07 13:22:16 +0000

    Implement nested inner and outer spans

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,28 +3,29 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run():
-    with tracer.start_as_current_span("home-activities-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.hubabuba", now.isoformat())
-      results = [{
-        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Smirnov Aleksey',
-        'message': 'Cloud is Future!',
-        'created_at': (now - timedelta(days=2)).isoformat(),
-        'expires_at': (now + timedelta(days=5)).isoformat(),
-        'likes_count': 5,
-        'replies_count': 1,
-        'reposts_count': 0,
-        'replies': [{
-          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'Worf',
-          'message': 'This post has no honor!',
-          'likes_count': 0,
-          'replies_count': 0,
+    with tracer.start_as_current_span("home-activities-data") as outer_span:
+        outer_span.set_attribute("outer", True)
+        span = trace.get_current_span()
+        now = datetime.now(timezone.utc).astimezone()
+        span.set_attribute("app.hubabuba", now.isoformat())
+        results = [{
+          'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+          'handle':  'Smirnov Aleksey',
+          'message': 'Cloud is Future!',
+          'created_at': (now - timedelta(days=2)).isoformat(),
+          'expires_at': (now + timedelta(days=5)).isoformat(),
+          'likes_count': 5,
+          'replies_count': 1,
           'reposts_count': 0,
-          'created_at': (now - timedelta(days=2)).isoformat()
+          'replies': [{
+            'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+            'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+            'handle':  'Worf',
+            'message': 'This post has no honor!',
+            'likes_count': 0,
+            'replies_count': 0,
+            'reposts_count': 0,
+            'created_at': (now - timedelta(days=2)).isoformat()
       }],
     },
     {
@@ -46,5 +47,8 @@ class HomeActivities:
       'replies': []
     }
     ]
-    span.set_attribute("app.result_length", now.date())
+        with tracer.start_as_current_span("home-results-activities") as inner_span:
+          inner_span.set_attribute("inner", True)
+          span = trace.get_current_span()
+          span.set_attribute("app.result_length", len(results))
     return results
