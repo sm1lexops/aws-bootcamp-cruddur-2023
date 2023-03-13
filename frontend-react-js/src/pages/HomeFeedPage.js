b6e733a3755e9b635b72ec61c4bc4d867a63d6bb
sm1lexops <62427708+sm1lexops@users.noreply.github.com>
2023-03-13 12:34:14 +0000

    update desktop/homefeed/profileinfo.js code

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -10,6 +10,9 @@ import ReplyForm from '../components/ReplyForm';
 // [TODO] Authenication
 import Cookies from 'js-cookie'
 
+// Implement AWS Amplify
+import { Auth } from 'aws-amplify';
+
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
   const [popped, setPopped] = React.useState(false);
@@ -35,17 +38,25 @@ export default function HomeFeedPage() {
     }
   };
 
-  const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
+// check if we are authenicated
+const checkAuth = async () => {
+  Auth.currentAuthenticatedUser({
+    // Optional, By default is false. 
+    // If set to true, this call will send a 
+    // request to Cognito to get the latest user data
+    bypassCache: false 
+  })
+  .then((user) => {
+    console.log('user',user);
+    return Auth.currentAuthenticatedUser()
+  }).then((cognito_user) => {
       setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
       })
-    }
-  };
-
+  })
+  .catch((err) => console.log(err));
+};
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
--- journal/week3.md
+++ journal/week3.md
@@ -33,3 +33,36 @@ Amplify.configure({
 });
 ```
 
+Conditionally show components based on logged in or logged out
+
+> Add to `HomeFeedPage.js`
+
+```sh
+mport { Auth } from 'aws-amplify';
+
+// set a state
+const [user, setUser] = React.useState(null);
+
+// check if we are authenicated
+const checkAuth = async () => {
+  Auth.currentAuthenticatedUser({
+    // Optional, By default is false. 
+    // If set to true, this call will send a 
+    // request to Cognito to get the latest user data
+    bypassCache: false 
+  })
+  .then((user) => {
+    console.log('user',user);
+    return Auth.currentAuthenticatedUser()
+  }).then((cognito_user) => {
+      setUser({
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
+      })
+  })
+  .catch((err) => console.log(err));
+};
+```
+
+
+
