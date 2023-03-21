ba2f74abf88986782fef038c5f079e38de0b48dd
sm1lexops <62427708+sm1lexops@users.noreply.github.com>
2023-03-21 13:51:00 +0000

    udpate home_activities.py and create_activity.py

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -11,31 +11,10 @@ class HomeActivities:
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.hubabuba", now.isoformat())
-      sql = query_wrap_array("""
-        SELECT
-          activities.uuid,
-          users.display_name,
-          users.handle,
-          activities.message,
-          activities.replies_count,
-          activities.reposts_count,
-          activities.likes_count,
-          activities.reply_to_activity_uuid,
-          activities.expires_at,
-          activities.created_at
-        FROM public.activities
-        LEFT JOIN public.users ON users.uuid = activities.user_uuid
-        ORDER BY activities.created_at DESC
-        """)
-      print(sql)
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
+      sql = db.template('activities','home')
+      results = db.query_array_json(sql)
       with tracer.start_as_current_span("home-results-activities") as inner_span:
         inner_span.set_attribute("inner", True)
         span = trace.get_current_span()
         span.set_attribute("app.result_length", len(sql))
-      return json[0]
+      return results
--- journal/week4.md
+++ journal/week4.md
@@ -888,3 +888,23 @@ class Db:
 
 db = Db()
 ```
+
+* Update `home_activities.py`
+
+```py
+class HomeActivities:
+  def run(cognito_user_id=None):
+    #logger.info("HomeActivities")
+    with tracer.start_as_current_span("home-activities-data") as outer_span:
+      outer_span.set_attribute("outer", True)
+      span = trace.get_current_span()
+      now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("app.hubabuba", now.isoformat())
+      sql = db.template('activities','home')
+      results = db.query_array_json(sql)
+      with tracer.start_as_current_span("home-results-activities") as inner_span:
+        inner_span.set_attribute("inner", True)
+        span = trace.get_current_span()
+        span.set_attribute("app.result_length", len(sql))
+      return results
+```
