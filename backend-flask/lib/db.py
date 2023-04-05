123fbae1ee238c7af7eb128106bf7e6e1e4f6510
sm1lexops <smiledevops@gmail.com>
2023-04-05 08:10:15 +0000

    update ENV to PROD

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -25,8 +25,7 @@ class Db:
 
   def init_pool(self):
     print("===os.getenv(CONNECTION_URL)::", os.getenv("CONNECTION_URL"))
-    connection_url = "postgresql://postgres:password@localhost:5432/cruddur"
-    #connection_url = os.getenv("CONNECTION_URL")
+    connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
     print("===== some issues =====>>>>" , connection_url)
   # we want to commit data such as an insert
