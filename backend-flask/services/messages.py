1fd1cafff69ea95f39604965608fbfda1c72b770
sm1lexops <smiledevops@gmail.com>
2023-04-05 07:45:48 +0000

    update service messages and message group

--- backend-flask/services/messages.py
+++ backend-flask/services/messages.py
@@ -10,16 +10,13 @@ class Messages:
     }
 
     sql = db.template('users','uuid_from_cognito_user_id')
-    my_user_uuid = db.query_value(sql,{
-      'cognito_user_id': cognito_user_id
-    })
-
-    print(f"UUID: {my_user_uuid}")
+    my_user_uuid = db.query_value(sql,{'cognito_user_id': cognito_user_id})
+    # TODO: we're suppose to check that we have permission to access
+    # this message_group_uuid, its missing in our access pattern.
 
     ddb = Ddb.client()
     data = Ddb.list_messages(ddb, message_group_uuid)
-    print("list_messages")
-    print(data)
+    print("list_messages:",data)
 
     model['data'] = data
     return model
