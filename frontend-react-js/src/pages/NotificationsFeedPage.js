2244e0243faeed3dc36bd9afea36018cfd083952
Aleksey Smirnov <62427708+sm1lexops@users.noreply.github.com>
2023-04-18 16:44:18 +0300

    Update NotificationsFeedPage.js

--- frontend-react-js/src/pages/NotificationsFeedPage.js
+++ frontend-react-js/src/pages/NotificationsFeedPage.js
@@ -7,6 +7,9 @@ import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
 
+// [TODO] Authenication
+import Cookies from 'js-cookie'
+
 export default function NotificationsFeedPage() {
   const [activities, setActivities] = React.useState([]);
   const [popped, setPopped] = React.useState(false);
@@ -19,9 +22,9 @@ export default function NotificationsFeedPage() {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/notifications`
       const res = await fetch(backend_url, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
-        },
+              headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`
+      },
         method: "GET"
       });
       let resJson = await res.json();
