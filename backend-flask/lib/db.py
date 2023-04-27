880bc437220dc536951d572f5f122c1d3546b188
sm1lexops <smiledevops@gmail.com>
2023-04-27 08:54:47 +0000

    fix connection pool-1

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -25,6 +25,7 @@ class Db:
 
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
+    print(connection_url)
     self.pool = ConnectionPool(connection_url)
   # we want to commit data such as an insert
   # be sure to check for RETURNING in all uppercases
