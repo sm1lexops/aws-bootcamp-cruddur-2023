6d768dab384f4cf5eccfae8df6d50d47114ce34c
sm1lexops <smiledevops@gmail.com>
2023-05-10 09:26:31 +0000

    fix messages time created

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -74,7 +74,8 @@ class Ddb:
       })
     return results
   def create_message(client,message_group_uuid, message, my_user_uuid, my_user_display_name, my_user_handle):
-    created_at = datetime.now().isoformat()
+    now = datetime.now(timezone.utc).astimezone().isoformat()
+    created_at = now
     message_uuid = str(uuid.uuid4())
 
     record = {
