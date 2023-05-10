5d759a787a8a9cde2312c8ed2f3cbfbb2b07e3f1
sm1lexops <smiledevops@gmail.com>
2023-05-10 10:17:31 +0000

    fix xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -270,7 +270,7 @@ def data_activities():
   return
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
-@xray_recorder.capture('activities_show')
+#@xray_recorder.capture('activities_show')
 def data_show_activity(activity_uuid):
   data = ShowActivity.run(activity_uuid=activity_uuid)
   return data, 200
